4f4516219651be279bafc7233a406f200dac5172
Nestor <nestor@HP-Pavilion-dv5-2133.(none)>
2012-05-06 18:40:25 -0300

    modificacion de conectar.php

--- web/funciones/conectar.php
+++ web/funciones/conectar.php
@@ -1,14 +1,26 @@
 <?php
-
-function consultar($query){
+$host="localhost";
+$db="pocholas-db";
+$user="root";
+$pass="Genius";
 try{
-$statement= $pdo->query($query);
+    $conectar=new PDO("mysql:host=$host;dbname=$db",$user,$pass);
+    $conectar->setAttribute(PDO::ATTR_EMULATE_PREPARES,true);
+    $conectar->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conectar->exec("SET NAMES UTF8");
 }  catch (PDOException $e){
-    echo 'Error en la consulta:'.$e->getMessage();
+   echo 'Error al Conectar a la Base de Datos: '.$e->getMessage();
 }
-//volcamos el objeto de tipo mysqli_result a un array asociativo
-$row=$statement->fetch(PDO::FETCH_ASSOC);
 
-return $row;
+//esta funcion toma cualquier consulta y devolvera un array con los datos
+function consultar($query){
+    try{
+    $statement= $pdo->query($query);
+    }  catch (PDOException $e){
+        echo 'Error en la consulta:'.$e->getMessage();
+    }
+    $row=$statement->fetch(PDO::FETCH_ASSOC);
+
+    return $row;
 }
 ?>
