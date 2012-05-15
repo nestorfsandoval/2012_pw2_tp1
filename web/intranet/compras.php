d6c4a7aaf0b5a2c513c9ac43497f666e1841be7d
leonardo <leonardoomar@yahoo.com.ar>
2012-05-14 22:46:38 -0300

    actualizando modulo compras de la intranet...

--- web/intranet/compras.php
+++ web/intranet/compras.php
@@ -1,17 +1,43 @@
+<?php
+require_once '../funciones/conectar.php';
+  if(isset($_GET['listar'])&& ($_GET['listar']== 'Listar'))
+  {
+    $lafecha = $_GET['desdefecha'];
+    preg_match( "/([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{2,4})/", $lafecha, $ftrunc); 
+    $fdesde=$ftrunc[3]."-".$ftrunc[2]."-".$ftrunc[1]; 
+    
+    $lafecha = $_GET['hastafecha'];
+    preg_match( "/([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{2,4})/", $lafecha, $ftrunc); 
+    $fhasta=$ftrunc[3]."-".$ftrunc[2]."-".$ftrunc[1]; 
+
+    $query = "SELECT * FROM compra WHERE fecha >= '".$fdesde."' AND fecha <='". $fhasta."'" ;
+    $resultados = consultar($query,$conectar);
+
+  }else{
+   $query = "SELECT * FROM compra" ;
+    $resultados = consultar($query,$conectar);
+    consultar($query,$conectar);
+  }
+
+?>
+
 <div class="cuerpo">
 	<fieldset>
 		<legend>Control de Stock</legend>
-                <form class="left" id="frm_btn_list" action="" method="get">
-			<input class="busqueda" type="text" name="busqueda" value="Ingrese Busqueda..." />
-			<label>Desde:</label>
-                        <input class="fecha" id="fechad" type="text" name="desdefecha" MAXLENGTH="10" value="dd/mm/aaaa"/>
-                        <label>Hasta:</label>
-			<input class="fecha" id="fechah" type="text" name="hastafecha" MAXLENGTH="10" value="dd/mm/aaaa"/><br/>
-			<input class="listar" id="btn_list" type="submit" name="listar" value="Listar"/>
+      <form class="left" id="frm_btn_list" action="index.php" method="GET">     
+        <input type="hidden" name="op" value="4">
+        <input class="busqueda" type="text" name="busqueda" value="Ingrese Busqueda..." />
+        <label>Desde:</label>
+        <input class="fecha" id="fechad" type="text" name="desdefecha" MAXLENGTH="10" value="dd/mm/aaaa"/>
+        <label>Hasta:</label>
+        <input class="fecha" id="fechah" type="text" name="hastafecha" MAXLENGTH="10" value="dd/mm/aaaa"/><br/>
+        <input class="listar" id="btn_list" type="submit" name="listar" value="Listar"/>
 		</form>
+    
 		<form class="right">
 			<input type="button" name="nuevocompra" value="Agregar Compra" id="nuevaCompra" />
 		</form>
+    
 		<form id="agregaCompra" class="panelUser" action="" method="POST" title="Agregar Compra">
 			<p id="titAlert" class="validateTips">Rellene todos los campos</p>
 				<label class="formulario">Proveedor:</label>
@@ -30,60 +56,16 @@
 		<tr class="titulos">
 			<td>FECHA</td><td>PROVEEDOR</td><td>N&deg; FACTURA</td><td>MONTO</td><td>REMITO</td><td>EDITAR</td>
 		</tr>
-		<tr class="listado">
-			<td>2011/09/30</td><td>Warner Music</td><td>0070632/11</td><td>$ 123772,00</td><td>6</td>
-			<td>
-				<img src="img/edit.gif" width="18" title="Editar" alt="Editar Carga" id="editCompra"/>
-			</td>
-		</tr>
-		<tr class="listado">
-			<td>2011/09/30</td><td>EMI</td><td>23776-11</td><td>$ 123772,00</td><td>45</td>
-			<td>
-				<img src="img/edit.gif" width="18" title="Editar" alt="Editar Carga" />
-			</td>
-		</tr>
-		<tr class="listado">
-			<td>2011/09/29</td><td>Elefant Records</td><td>001543-11-7</td><td>$ 123772,00</td><td>33</td>
-			<td>
-				<img src="img/edit.gif" width="18" title="Editar" alt="Editar Carga" />
-			</td>
-		</tr>
-		<tr class="listado">
-			<td>2011/09/1</td><td>EMI</td><td>22299-11</td><td>$ 123772,00</td><td>30</td>
-			<td>
-				<img src="img/edit.gif" width="18" title="Editar" alt="Editar Carga" />
-			</td>
-		</tr>
-		<tr class="listado">
-			<td>2011/09/1</td><td>Warner Music</td><td>0063645/11</td><td>$ 123772,00</td><td>26</td>
-			<td>
-				<img src="img/edit.gif" width="18" title="Editar" alt="Editar Carga" />
-			</td>
-		</tr>
-		<tr class="listado">
-			<td>2011/09/1</td><td>Sony Music</td><td>00029265/8</td><td>$ 123772,00</td><td>22</td>
-			<td>
-				<img src="img/edit.gif" width="18" title="Editar" alt="Editar Carga" />
-			</td>
-		</tr>
-		<tr class="listado">
-			<td>2011/08/28</td><td>Elefant Records</td><td>001652-11-8</td><td>$ 123772,00</td><td>20</td>
-			<td>
-				<img src="img/edit.gif" width="18" title="Editar" alt="Editar Carga" />
-			</td>
-		</tr>
-		<tr class="listado">
-			<td>2011/08/28</td><td>EMI</td><td>21821-11</td><td>$ 123772,00</td><td>18</td>
-			<td>
-				<img src="img/edit.gif" width="18" title="Editar" alt="Editar Carga" />
-			</td>
-		</tr>
-		<tr class="listado">
-			<td>2011/08/15</td><td>Warner Music</td><td>0060932/11</td><td>$ 123772,00</td><td>11</td>
-			<td>
-				<img src="img/edit.gif" width="18" title="Editar" alt="Editar Carga" />
-			</td>
-		</tr>
+    
+    <?php foreach($resultados as $fila)
+    {
+      echo "<tr>";
+        foreach($fila as $campito){
+          echo "<td>".$campito."</td>";
+        }
+      echo "</tr>";
+    }
+    ?>
 		</table>
 	</fieldset>
 </div>
